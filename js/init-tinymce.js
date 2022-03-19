4c60e9778950998e9c1ad81fd71a4c228a0185ad
Glebap <90100712+Glebap@users.noreply.github.com>
2022-03-19 14:21:21 +0200

    1

---
+++ js/init-tinymce.js
@@ -0,0 +1,7 @@
+tinymce.init({
+  selector: '#mytextarea',
+  plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code',
+  toolbar: ' bold | italic | underline | code',
+  height: 730,
+  width: 772,
+});
